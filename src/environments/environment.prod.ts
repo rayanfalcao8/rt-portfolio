63ee81922efe379a85bd610a1eeef3cedfcd7ba8
Rayan TSOLEFACK <rayanfalcao8@gmail.com>
2023-02-08 12:25:51 +0100

    Build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBzeGGpPBzZoitPNr-oigXntLNg2npbPUs",
+    authDomain: "portfolio-6bb86.firebaseapp.com",
+    databaseURL: "https://portfolio-6bb86.firebaseio.com",
+    projectId: "portfolio-6bb86",
+    storageBucket: "portfolio-6bb86.appspot.com",
+    messagingSenderId: "938238210797",
+    appId: "1:938238210797:web:999784c3d15de2d7df92e4"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBzeGGpPBzZoitPNr-oigXntLNg2npbPUs",
+    authDomain: "portfolio-6bb86.firebaseapp.com",
+    databaseURL: "https://portfolio-6bb86.firebaseio.com",
+    projectId: "portfolio-6bb86",
+    storageBucket: "portfolio-6bb86.appspot.com",
+    messagingSenderId: "938238210797",
+    appId: "1:938238210797:web:999784c3d15de2d7df92e4"
+  }
 };
 
 /*
